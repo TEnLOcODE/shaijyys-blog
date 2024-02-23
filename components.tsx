4e92458fc2d7c6409348fbcd1f2a66c2563d5e82
Eitan Biletski <87935299+TEnLOcODE@users.noreply.github.com>
2024-02-23 20:47:40 +0200

    Update components.tsx

--- components.tsx
+++ components.tsx
@@ -376,7 +376,7 @@ function IconX() {
   return (
    
       <svg xmlns="http://www.w3.org/2000/svg">
- <path d="m236 0h46l-101 115 118 156h-92.6l-72.5-94.8-83 94.8h-46l107-123-113-148h94.9l65.5 86.6zm-16.1 244h25.5l-165-218h-27.4z" fill="currentColor"/>
+ <path d="m236 0h46l-101 115 118 156h-92.6l-72.5-94.8-83 94.8h-46l107-123-113-148h94.9l65.5 86.6zm-16.1 244h25.5l-165-218h-27.4z"/>
 <style xmlns="" id="autoconsent-prehide"/></svg>
     
   );
